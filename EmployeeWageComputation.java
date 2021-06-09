817f78d61845560ea17d9ab62ac6f406afefb2f9
Sayani Koley <sayanikoley8@gmail.com>
2021-06-09 11:51:26 +0530

    Added calculation of Daily Employee Wage part time

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,7 +2,10 @@ public class EmployeeWageComputation {
 	public static void main(String args[]) {
 		int empWagePerHour = 20;
 		int fullDayHour = 8;
-		int empDailyWage = (empWagePerHour * fullDayHour);
-		System.out.println("Daily wage of the employee is: Rs."+empDailyWage);
+		int parttimeDayHour = 4;
+		int empDailyWageFulltime = (empWagePerHour * fullDayHour);
+		System.out.println("Daily wage of the employee working full time is: Rs."+empDailyWageFulltime);
+		int empDailyWageParttime = (empWagePerHour * parttimeDayHour);
+		System.out.println("Daily wage of the employee working part time is: Rs."+empDailyWageParttime);
 	}
 }
