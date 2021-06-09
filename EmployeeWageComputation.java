c8c23e81a6066f2f19ccea0c1bec8827403aa3d4
Sayani Koley <sayanikoley8@gmail.com>
2021-06-09 12:51:13 +0530

    Added wages computation for a month

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,28 +4,33 @@ public class EmployeeWageComputation {
 	public static void main(String args[]) {
 		int empPresent = 1;
 		int empWagePerHour = 20;
+		int workingDaysPerMonth = 20;
 		Scanner sc = new Scanner(System.in);
 		int empCheck = (int)(Math.floor(Math.random() * 10)) % 2;
 		switch(empCheck)
 		{
 			case 0:
 				System.out.println("Employee is absent today");
-				System.out.println("The daily wage of the employee cannot be calculated since the employee is absent!");
+				System.out.println("The wage of the employee cannot be calculated since the employee is absent!");
 				break;
 			case 1:
 				System.out.println("Employee is present today");
 				System.out.print("Enter 8 for Fulltime presence or 4 for Parttime presence: ");
 				int time = sc.nextInt();
-				System.out.println("You were present for " +time+ " hours today.");
 				switch(time)
 				{
 					case 4:
-						int empDailyWageFullTime = (empWagePerHour * time);
-						System.out.println("Daily wage of the employee working fulltime is: Rs."+empDailyWageFullTime);
+						System.out.println("You were present for " +time+ " hours today.");
+						int empDailyWagePartTime = (empWagePerHour * time * workingDaysPerMonth);
+						System.out.println("Wages per month of the employee working parttime is: Rs."+empDailyWagePartTime);
 						break;
 					case 8:
-						int empDailyWagePartTime = (empWagePerHour * time);
-						System.out.println("Daily wage of the employee working parttime is: Rs."+empDailyWagePartTime);
+						System.out.println("You were present for " +time+ " hours today.");
+						int empDailyWageFullTime = (empWagePerHour * time * workingDaysPerMonth);
+						System.out.println("Wages per month of the employee working fulltime is: Rs."+empDailyWageFullTime);
+						break;
+					default:
+						System.out.println("Wrong input! Employee Wage cannot be calculated!");
 						break;
 				}
 				break;
