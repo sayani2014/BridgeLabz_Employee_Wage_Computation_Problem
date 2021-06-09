08757870c4267bc41ffc3111fb54bdd575b837f8
Sayani Koley <sayanikoley8@gmail.com>
2021-06-09 17:05:30 +0530

    Added part time computation

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,9 +3,23 @@ public class EmployeeWageComputation {
 		int empWagePerHour = 20;
 		int fullDayHour = 8;
 		int parttimeDayHour = 4;
-		int empDailyWageFulltime = (empWagePerHour * fullDayHour);
-		System.out.println("Daily wage of the employee working full time is: Rs."+empDailyWageFulltime);
-		int empDailyWageParttime = (empWagePerHour * parttimeDayHour);
-		System.out.println("Daily wage of the employee working part time is: Rs."+empDailyWageParttime);
+		int empPresentFullDay = 1;
+		int empAbsent = 0;
+		int empCheck = (int)(Math.floor(Math.random() * 10)) % 3;
+		if (empCheck == empPresentFullDay) {
+			System.out.println("Employee is present for full time");
+			int empDailyWageFulltime = (empWagePerHour * fullDayHour);
+			System.out.println("Daily wage of the employee working full time is: Rs."+empDailyWageFulltime);
+		}
+		else if (empCheck == empAbsent) {
+			System.out.println("Employee is absent");
+			System.out.println("Wage of the employee for the day is zero");
+		}
+		else {
+			System.out.println("Employee is present for part time");
+			int empDailyWageParttime = (empWagePerHour * parttimeDayHour);
+			System.out.println("Daily wage of the employee working part time is: Rs."+empDailyWageParttime);
+		}
 	}
 }
+
