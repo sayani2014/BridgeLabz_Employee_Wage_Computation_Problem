680658386973ceed11597e35f3d2b8bd38031eb5
Sayani Koley <38989758+sayani2014@users.noreply.github.com>
2021-06-09 17:41:36 +0530

    Calculated salary of the employee for a month

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,38 +2,31 @@ import java.util.*;
 
 public class EmployeeWageComputation {
 	public static void main(String args[]) {
-		int empPresent = 1;
 		int empWagePerHour = 20;
 		int workingDaysPerMonth = 20;
-		Scanner sc = new Scanner(System.in);
-		int empCheck = (int)(Math.floor(Math.random() * 10)) % 2;
-		switch(empCheck)
-		{
-			case 0:
-				System.out.println("Employee is absent today");
-				System.out.println("The wage of the employee cannot be calculated since the employee is absent!");
-				break;
-			case 1:
-				System.out.println("Employee is present today");
-				System.out.print("Enter 8 for Fulltime presence or 4 for Parttime presence: ");
-				int time = sc.nextInt();
-				switch(time)
-				{
-					case 4:
-						System.out.println("You were present for " +time+ " hours today.");
-						int empDailyWagePartTime = (empWagePerHour * time * workingDaysPerMonth);
-						System.out.println("Wages per month of the employee working parttime is: Rs."+empDailyWagePartTime);
-						break;
-					case 8:
-						System.out.println("You were present for " +time+ " hours today.");
-						int empDailyWageFullTime = (empWagePerHour * time * workingDaysPerMonth);
-						System.out.println("Wages per month of the employee working fulltime is: Rs."+empDailyWageFullTime);
-						break;
-					default:
-						System.out.println("Wrong input! Employee Wage cannot be calculated!");
-						break;
-				}
-				break;
+		int fullDayHour = 8;
+		int parttimeDayHour = 4;
+		int empWage;
+		int amount = 0;
+		int no_of_days = 20;
+		for(int i=1;i<=no_of_days;i++) {
+			int empCheck = (int)(Math.floor(Math.random() * 10)) % 3;
+			switch(empCheck){
+				case 0:
+                                	break;
+				case 1:
+                                	empWage = empWagePerHour * fullDayHour;
+					amount = amount + empWage;
+					break;
+				case 2:
+                        		empWage = empWagePerHour * parttimeDayHour;
+					amount = amount + empWage;
+					break;
+				default:
+					System.out.println("Computation Failed! Please check for details!");
+					break;
+			}
 		}
+		System.out.println("Wages per month of the employee is : Rs."+amount);
 	}
 }
