0657b0eb82f4151de31f32cabf842fc2a26e5ecf
Sayani Koley <sayanikoley8@gmail.com>
2021-06-09 16:42:25 +0530

    Upated Daily Wage Computation

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,7 +2,16 @@ public class EmployeeWageComputation {
 	public static void main(String args[]) {
 		int empWagePerHour = 20;
 		int fullDayHour = 8;
-		int empDailyWage = (empWagePerHour * fullDayHour);
-		System.out.println("Daily wage of the employee is: Rs."+empDailyWage);
+		int empPresent = 1;
+		int empCheck = (int)(Math.floor(Math.random() * 10)) % 2;
+		if (empCheck == empPresent) {
+			System.out.println("Employee is present");
+			int empDailyWage = (empWagePerHour * fullDayHour);
+			System.out.println("Daily wage of the employee is: Rs."+empDailyWage);
+		}
+		else {
+			System.out.println("Employee is absent");
+			System.out.println("Wage of the employee for the day is zero");
+		}
 	}
 }
