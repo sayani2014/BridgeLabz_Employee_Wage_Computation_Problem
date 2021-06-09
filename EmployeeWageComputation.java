813c1b4283d9da5cdbc33056d56b3bbaa05ff9f9
Sayani Koley <38989758+sayani2014@users.noreply.github.com>
2021-06-09 17:21:11 +0530

    Computation solved using switch case

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 public class EmployeeWageComputation {
 	public static void main(String args[]) {
-		int empPresent = 1;
 		int empWagePerHour = 20;
-		Scanner sc = new Scanner(System.in);
-		int empCheck = (int)(Math.floor(Math.random() * 10)) % 2;
+		int fullDayHour = 8;
+		int parttimeDayHour = 4;
+		int empCheck = (int)(Math.floor(Math.random() * 10)) % 3;
 		switch(empCheck)
 		{
 			case 0:
@@ -13,25 +13,20 @@ public class EmployeeWageComputation {
 				System.out.println("The daily wage of the employee cannot be calculated since the employee is absent!");
 				break;
 			case 1:
-				System.out.println("Employee is present today");
-				System.out.print("Enter 8 for Fulltime presence or 4 for Parttime presence: ");
-				int time = sc.nextInt();
-				System.out.println("You were present for " +time+ " hours today.");
-				switch(time)
-				{
-					case 4:
-						int empDailyWagePartTime = (empWagePerHour * time);
-						System.out.println("Daily wage of the employee working parttime is: Rs."+empDailyWagePartTime);
-						break;
-					case 8:
-						int empDailyWageFullTime = (empWagePerHour * time);
-						System.out.println("Daily wage of the employee working fulltime is: Rs."+empDailyWageFullTime);
-						break;
-					default:
-						System.out.println("Wrong input! Employee Wage cannot be calculated!");
-						break;
-				}
+				System.out.println("Employee is present for full time today");
+				int empDailyWageFullTime = (empWagePerHour * fullDayHour);
+				System.out.println("Daily wage of the employee working fulltime is: Rs."+empDailyWageFullTime);
+				break;
+			case 2:
+				System.out.println("Employee is present for part time today");
+				int empDailyWagePartTime = (empWagePerHour * parttimeDayHour);
+				System.out.println("Daily wage of the employee working parttime is: Rs."+empDailyWagePartTime);
+				break;
+			default:
+				System.out.println("Computation Failed! Please check for details!");
 				break;
 		}
 	}
 }
+
+		
