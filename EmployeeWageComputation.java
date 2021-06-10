650c3831d223abd134450a88e41c668dd0faa8c3
Sayani Koley <38989758+sayani2014@users.noreply.github.com>
2021-06-10 16:27:45 +0530

    Updated Calculate Daily Employee Wage using Switch Case for a complete month

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,32 +1,45 @@
-import java.util.*;
+/* Calculate Daily Employee Wage using Switch Case for a complete month
+ * Working Hour Full Time = 8
+ * Working Hour Part Time = 4
+ * Wage Per Hour = 20
+ * Number of Days = 20
+ *
+ * @author: SAYANI KOLEY
+ * DATE: 10/06/2021
+ */
 
 public class EmployeeWageComputation {
-	public static void main(String args[]) {
-		int empWagePerHour = 20;
-		int workingDaysPerMonth = 20;
-		int fullDayHour = 8;
-		int parttimeDayHour = 4;
-		int empWage;
-		int amount = 0;
-		int no_of_days = 20;
-		for(int i=1;i<=no_of_days;i++) {
-			int empCheck = (int)(Math.floor(Math.random() * 10)) % 3;
-			switch(empCheck){
-				case 0:
-          				break;
-				case 1:
-          				empWage = empWagePerHour * fullDayHour;
-					amount = amount + empWage;
-					break;
-				case 2:
-         				empWage = empWagePerHour * parttimeDayHour;
-					amount = amount + empWage;
-					break;
-				default:
-					System.out.println("Computation Failed! Please check for details!");
-					break;
-			}
-		}
-		System.out.println("Wages per month of the employee is : Rs."+amount);
-	}
+    public static final int absent = 0;
+    public static final int fullTime = 1;
+    public static final int partTime = 2;
+    public static final int no_of_days = 20;
+    public static final int empWagePerHour = 20;
+
+    public static void main(String args[]) {
+        int empHour = 0;
+        int amount = 0;
+        for(int i=1;i<=no_of_days;i++) {
+            int empCheck = (int) (Math.floor(Math.random() * 10)) % 3;
+            switch (empCheck) {
+                case absent:
+                    System.out.println("Employee is absent today");
+                    empHour = 0;
+                    break;
+                case fullTime:
+                    System.out.println("Employee is present for full time today");
+                    empHour = 8;
+                    break;
+                case partTime:
+                    System.out.println("Employee is present for part time today");
+                    empHour = 4;
+                    break;
+            }
+            int empDailyWage = (empWagePerHour * empHour);
+            amount += empDailyWage;
+        }
+        System.out.println("Wages per month of the employee is : Rs."+amount);
+    }
 }
+
+
+
