dd16691eaf7e73a991d31a449788ff0711c11f64
Sayani Koley <38989758+sayani2014@users.noreply.github.com>
2021-06-10 16:09:02 +0530

    Updated with part time wage calculation

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,25 +1,40 @@
-public class EmployeeWageComputation {
-	public static void main(String args[]) {
-		int empWagePerHour = 20;
-		int fullDayHour = 8;
-		int parttimeDayHour = 4;
-		int empPresentFullDay = 1;
-		int empAbsent = 0;
-		int empCheck = (int)(Math.floor(Math.random() * 10)) % 3;
-		if (empCheck == empPresentFullDay) {
-			System.out.println("Employee is present for full time");
-			int empDailyWageFulltime = (empWagePerHour * fullDayHour);
-			System.out.println("Daily wage of the employee working full time is: Rs."+empDailyWageFulltime);
-		}
-		else if (empCheck == empAbsent) {
-			System.out.println("Employee is absent");
-			System.out.println("Wage of the employee for the day is zero");
-		}
-		else {
-			System.out.println("Employee is present for part time");
-			int empDailyWageParttime = (empWagePerHour * parttimeDayHour);
-			System.out.println("Daily wage of the employee working part time is: Rs."+empDailyWageParttime);
-		}
-	}
-}
+/* Calculate Daily Employee Wage
+ * Working Hour Full Time = 8
+ * Working Hour Part Time = 4
+ * Wage Per Hour = 20
+ *
+ * @author: SAYANI KOLEY
+ * DATE: 10/06/2021
+ */
+
+public class SnakeAndLadderProblem {
+        public static void main(String args[]) {
+            int empWagePerHour = 20;
+            int empHour = 0;
+            int empPresentFullDay = 1;
+            int empAbsent = 0;
+            int empCheck = (int)(Math.floor(Math.random() * 10)) % 3;
+            if (empCheck == empPresentFullDay) {
+                System.out.println("Employee is present for full time");
+                empHour = 8;
+            }
+            else if (empCheck == empAbsent) {
+                System.out.println("Employee is absent");
+                empHour = 0;
+            }
+            else {
+                System.out.println("Employee is present for part time");
+                empHour = 4;
+            }
+            wageCalculation(empWagePerHour, empHour);
+        }
+        private static void wageCalculation(int empWagePerHour, int empHour) {
+            int empDailyWage = (empWagePerHour * empHour);
+            System.out.println("Daily wage of the employee is: Rs."+empDailyWage);
+        }
+    }
+
+
+
+
 
