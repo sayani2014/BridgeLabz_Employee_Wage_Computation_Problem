24e4fde213eaa42e9cb257343cee92a377464c31
Sayani Koley <38989758+sayani2014@users.noreply.github.com>
2021-06-10 16:03:19 +0530

    Updated Daily Wage Calculation of a employee

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,18 +1,32 @@
+/* Calculate Daily Employee Wage
+ * Working Hour = 8
+ * Wage Per Hour = 20
+ *
+ * @author: SAYANI KOLEY
+ * DATE: 10/06/2021
+ */
+
 public class EmployeeWageComputation {
-	public static void main(String args[]) {
-		int empWagePerHour = 20;
-		int fullDayHour = 8;
-		int empPresent = 1;
-		int empCheck = (int)(Math.floor(Math.random() * 10)) % 2;
-		if (empCheck == empPresent) {
-			System.out.println("Employee is present");
-			int empDailyWage = (empWagePerHour * fullDayHour);
-			System.out.println("Daily wage of the employee is: Rs."+empDailyWage);
-		}
-		else {
-			System.out.println("Employee is absent");
-			System.out.println("Wage of the employee for the day is zero");
-		}
-	}
-}
+        public static void main(String args[]) {
+            int empWagePerHour = 20;
+            int empHour = 0;
+            int empPresent = 1;
+            int empCheck = (int)(Math.floor(Math.random() * 10)) % 2;
+            if (empCheck == empPresent) {
+                System.out.println("Employee is present");
+                empHour = 8;
+            }
+            else {
+                System.out.println("Employee is absent");
+                empHour = 0;
+            }
+            wageCalculation(empWagePerHour, empHour);
+        }
+        private static void wageCalculation(int empWagePerHour, int empHour) {
+            int empDailyWage = (empWagePerHour * empHour);
+            System.out.println("Daily wage of the employee is: Rs."+empDailyWage);
+        }
+    }
+
+
 
