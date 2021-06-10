f9182c9b41b90d108109dbec87ce17ec0f3c80be
Sayani Koley <38989758+sayani2014@users.noreply.github.com>
2021-06-10 16:10:29 +0530

    Updated check whether employee is present or absent

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,3 +1,9 @@
+/* Check whether the employee is present or absent
+ *
+ * @author: SAYANI KOLEY
+ * DATE: 10/06/2021
+ */
+
 public class EmployeeWageComputation {
 	public static void main(String args[]) {
 		int empPresent = 1;
